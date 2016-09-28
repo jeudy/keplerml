30814ff424a50f845c71369dd24b39109dc87389
Christian <christian.asch4@gmail.com>
2016-09-28 09:31:41 -0600

    Se agregaron las listas de confirmed y falsos positivos, ademas de metodos para chequear si el fits que se esta insertando a la db pertenece a alguna de estas categorias

--- stars_db/get_metadata.py
+++ stars_db/get_metadata.py
@@ -4,30 +4,30 @@ from os.path import join
 from astropy.io import fits
 
 def get_list_of_dicts(dire):
-	"""
-	Obtiene las listas de diccionarios para uso con las funciones de stars_db_manager
+    """
+    Obtiene las listas de diccionarios para uso con las funciones de stars_db_manager
 
-	(str) dire: Directorio en donde se encuentran los fits
+    (str) dire: Directorio en donde se encuentran los fits
 
-	"""
-	list_of_dicts_lightcurve_metadata = []
-	list_of_dicts_metadata = []
-	archivos = listdir(dire)
-	for i in range(len(archivos)):
-		archivos[i] = join(dire, archivos[i])
-	for archivo in archivos:
-		list_of_dicts_lightcurve_metadata.append(get_star_lightcurve_metadata(archivo))
-		list_of_dicts_metadata.append(get_star_metadata(archivo))
-	return (list_of_dicts_metadata, list_of_dicts_lightcurve_metadata)
+    """
+    list_of_dicts_lightcurve_metadata = []
+    list_of_dicts_metadata = []
+    archivos = listdir(dire)
+    for i in range(len(archivos)):
+        archivos[i] = join(dire, archivos[i])
+    for archivo in archivos:
+        list_of_dicts_lightcurve_metadata.append(get_star_lightcurve_metadata(archivo))
+        list_of_dicts_metadata.append(get_star_metadata(archivo))
+    return (list_of_dicts_metadata, list_of_dicts_lightcurve_metadata)
 
 def get_star_lightcurve_metadata(fits_arch):
-	"""
-	Obtiene los metadatos de la curva de luz de un archivo fits para uso con las funciones de stars_db_manager
+    """
+    Obtiene los metadatos de la curva de luz de un archivo fits para uso con las funciones de stars_db_manager
 
-	(str) fits_arch: Archivo del que se obtendran los metadatos
-	"""
-	hdu = fits.open(fits_arch)
-	lightcurve_metadata = {'kepler_id':hdu[1].header["KEPLERID"],
+    (str) fits_arch: Archivo del que se obtendran los metadatos
+    """
+    hdu = fits.open(fits_arch)
+    lightcurve_metadata = {'kepler_id':hdu[1].header["KEPLERID"],
                     'source_filename':fits_arch,
                     'exposure':hdu[1].header["EXPOSURE"],
                     'bjd_ref_integer':hdu[1].header["BJDREFI"],
@@ -60,21 +60,21 @@ def get_star_lightcurve_metadata(fits_arch):
                     'short_cadence_fixed_offset':hdu[1].header["SCFXDOFF"],
                     'crowd_sap':hdu[1].header["CROWDSAP"],
                     'target_variability':hdu[1].header["PDCVAR"]}
-	hdu.close()
-	return lightcurve_metadata
+    hdu.close()
+    return lightcurve_metadata
 
 
 def get_star_metadata(fits_arch):
-	"""
-	Obtiene los metadatos de un archivo fits para uso con las funciones de stars_db_manager
+    """
+    Obtiene los metadatos de un archivo fits para uso con las funciones de stars_db_manager
 
-	(str) fits_arch: Archivo del que se obtendran los metadatos
-	"""
-	hdu = fits.open(fits_arch)
-	metadata = {'kepler_id':hdu[0].header["KEPLERID"],
+    (str) fits_arch: Archivo del que se obtendran los metadatos
+    """
+    hdu = fits.open(fits_arch)
+    metadata = {'kepler_id':hdu[0].header["KEPLERID"],
                     'source_filename':fits_arch,
-                    'confirmed':False, #por ahora
-                    'negative':False,  #por ahora
+                    'confirmed':check_if_confirmed(hdu[0].header["KEPLERID"]),
+                    'negative':check_if_false_positive(hdu[0].header["KEPLERID"),
                     'channel':hdu[0].header["CHANNEL"],
                     'sky_group':hdu[0].header["SKYGROUP"],
                     'module':hdu[0].header["MODULE"],
@@ -101,5 +101,19 @@ def get_star_metadata(fits_arch):
                     'fe_h_metallicity':hdu[0].header["FEH"],
                     'radius':hdu[0].header["RADIUS"],
                     'load_timestamp':hdu[0].header["DATE"]}
-	hdu.close()
-	return metadata
+    hdu.close()
+    return metadata
+
+def check_if_confirmed(kplr_id)
+    with open('Kepler_confirmed_list.txt', 'r') as f:
+        for lines in f:
+            if "kplr{zeroes}{ids}".format(zeroes='0'*(9-len(kplr_id)), ids=kplr_id) == lines.strip():
+                return True
+    return False
+
+def check_if_false_positive(kplr_id)
+    with open('false_positives_list.txt', 'r') as f:
+        for lines in f:
+            if "kplr{zeroes}{ids}".format(zeroes='0'*(9-len(kplr_id)), ids=kplr_id) == lines.strip():
+                return True
+    return False
